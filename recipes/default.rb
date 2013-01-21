e887c8d5fd8310f18d1eb80537de950abdbe04bb
Mark Burton <mark@greensocs.com>
2013-01-21 10:21:26 +0100

    Use git for greenlib install

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,8 @@
 
 package "build-essential"
 package "cmake"
-package "libboost1.49-dev"
+package "libboost-filesystem1.49-dev"
+#package "libboost1.49-dev"
 
 
 directory "/vagrant/ModelLibrary/greensocs" do
@@ -22,26 +23,62 @@ directory "/vagrant/ModelLibrary/greensocs" do
   recursive true
 end
 
-remote_file Chef::Config[:file_cache_path]+"/greenlib-1.0.0-Source.tar.gz" do
-  not_if {File.exists?('/vagrant/ModelLibrary/greensocs/include')}
-  source "http://www.greensocs.com/files/greenlib-1.0.0-Source.tar.gz"
-  mode "0644"
-  action :create_if_missing
-end
 
-bash "get GreenLib" do
-  cwd Chef::Config[:file_cache_path]
+bash "Checkout GreenLib"
   code <<-EOH
-
-  tar xzf greenlib-1.0.0-Source.tar.gz
-  mkdir greenlib.build
-  cd greenlib.build
-
-  export SYSTEMC_HOME=/usr/local/systemc-2.3.0
-
-  cmake -DCMAKE_INSTALL_PREFIX=/vagrant/ModelLibrary/greensocs ../greenlib-1.0.0-Source/
-  make install
-
+# need to specify branch
+    git clone  git://git.greensocs.com/greenlib  /vagrant/ModelLibrary/greensocs/greenlib.source
   EOH
-  creates "/vagrant/ModelLibrary/greensocs/include"
+  creates "/vagrant/ModelLibrary/greensocs/greenlib.source"
+  environment ({ 'http_proxy' => Chef::Config[:http_proxy] })
 end
+
+bash "Update GreenLib" do
+  code <<-EOH
+    cd /vagrant/ModelLibrary/greensocs/greenlib.source
+    git pull origin master
+  EOH
+  environment ({ 'http_proxy' => Chef::Config[:http_proxy] })
+end
+
+ruby_block "compile GreenLib" do
+  block do
+    IO.popen( <<-EOH
+       cd /vagrant/ModelLibrary/greensocs
+       mkdir greenlib.build
+       cd greenlib.build
+
+       export SYSTEMC_HOME=/usr/local/systemc-2.3.0
+
+       cmake -DCMAKE_INSTALL_PREFIX=/vagrant/ModelLibrary/greensocs ../greenlib.source/
+       make install
+     EOH
+   ) { |f|  f.each_line { |line| puts line } }
+  end
+end
+
+
+
+# remote_file Chef::Config[:file_cache_path]+"/greenlib-1.0.0-Source.tar.gz" do
+#   not_if {File.exists?('/vagrant/ModelLibrary/greensocs/include')}
+#   source "http://www.greensocs.com/files/greenlib-1.0.0-Source.tar.gz"
+#   mode "0644"
+#   action :create_if_missing
+# end
+
+# bash "get GreenLib" do
+#   cwd Chef::Config[:file_cache_path]
+#   code <<-EOH
+
+#   tar xzf greenlib-1.0.0-Source.tar.gz
+#   mkdir greenlib.build
+#   cd greenlib.build
+
+#   export SYSTEMC_HOME=/usr/local/systemc-2.3.0
+
+#   cmake -DCMAKE_INSTALL_PREFIX=/vagrant/ModelLibrary/greensocs ../greenlib-1.0.0-Source/
+#   make install
+
+#   EOH
+#   creates "/vagrant/ModelLibrary/greensocs/include"
+# end
