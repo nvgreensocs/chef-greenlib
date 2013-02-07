dd039ac35a48768c5435dc0e81e668cb735ae1d3
Mark Burton <mark@greensocs.com>
2013-02-07 20:13:07 +0100

    3rd attempt at sourcing profiles

--- recipes/default.rb
+++ recipes/default.rb
@@ -44,7 +44,7 @@ end
 ruby_block "compile GreenLib" do
   block do
      IO.popen(  <<-EOH
-       . #{node[:prefix]}/bash.profile.d/*
+       for i in #{node[:prefix]}/bash.profile.d/*; do . $i; done
        # the profile should now include SystemC export SYSTEMC_HOME=/usr/local/systemc-2.3.0
 
        cd #{node[:prefix]}/ModelLibrary/greensocs
